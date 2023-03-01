6ef3a962aa0100726182274e3a053a2ea6a5e1e3
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2023-03-01 19:17:04 +0530

    orders,transaction req files fixes

---
+++ database/migrations/2023_03_01_062339_create_order_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->float('taxable_amount');
+            $table->float('cgst_percent');
+            $table->float('cgst');
+            $table->float('sgst_percent');
+            $table->float('sgst');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
