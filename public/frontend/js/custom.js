93ec81cf5a09e7b1cebf123eaeca20366c79bb63
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-08 17:01:59 +0530

    slider in custome class

--- public/frontend/js/custom.js
+++ public/frontend/js/custom.js
@@ -1,4 +1,64 @@
-// $('.tabs-sliderlg').slick({
-//     prevArrow: false,
-//     nextArrow: false
-//   });
+jQuery('.tabs-sliderlg').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 5,
+            }
+          },
+        {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+            }
+          },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+    ]
+});
+
+
+jQuery('.fragrances').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+     
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+    ]
+});
+
