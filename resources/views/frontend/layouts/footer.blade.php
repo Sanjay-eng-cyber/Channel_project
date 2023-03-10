2321283111a6b0e2e70166456450ae2c3b0c485c
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-10 14:26:59 +0530

    footer updated

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,165 +1,186 @@
  <!-- footer of the Page -->
  <footer id="mt-footer" class="style2 wow fadeInUp custome-footer" data-wow-delay="0.6s">
-    <!-- F Promo Box of the Page -->
-    <aside class="f-promo-box dark">
-        <div class="container divider">
-            <div class="row row-cols-2 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4 ">
-                <div class="col mt-paddingbottomsm ft-svg-class-1">
-                    <!-- F Widget Item of the Page -->
-                    <div class="f-widget-item ft-svg-sub-class-1">
-                        <span class="widget-icon ft-svg-child-1">
-                            <img src="{{asset('frontend/images/products/footer-icon/free-shipping.svg')}}" alt="">
-
-                        </span>
-                        <div class="txt-holder  ft-svg-child-2">
-                            <h1 class="f-promo-box-heading">FREE SHIPPING</h1>
-                            <p>Free shipping on all US order</p>
-                        </div>
-                    </div>
-                    <!-- F Widget Item of the Page end -->
-                </div>
-                <div class="col mt-paddingbottomsm ft-svg-class-2">
-                    <!-- F Widget Item of the Page -->
-                    <div class="f-widget-item ft-svg-sub-class-2">
-                        <span class="widget-icon ft-svg-child-1">
-                            <img src="{{asset('frontend/images/products/footer-icon/support.svg')}}" alt="">
-                        </span>
-                        <div class="txt-holder ft-svg-child-2">
-                            <h1 class="f-promo-box-heading">SUPPORT 24/7</h1>
-                            <p>We support 24 hours a day</p>
-                        </div>
-                    </div>
-                    <!-- F Widget Item of the Page -->
-                </div>
-                <div class="col mt-paddingbottomxs ft-svg-class-3">
-                    <!-- F Widget Item of the Page -->
-                    <div class="f-widget-item ft-svg-sub-class-3">
+     <!-- F Promo Box of the Page -->
+     <aside class="f-promo-box dark">
+         <div class="container divider">
+             <div class="row row-cols-2 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4 ">
+                 <div class="col mt-paddingbottomsm ft-svg-class-1">
+                     <!-- F Widget Item of the Page -->
+                     <div class="f-widget-item ft-svg-sub-class-1">
                          <span class="widget-icon ft-svg-child-1">
-                            <img src="{{asset('frontend/images/products/footer-icon/gift-card.svg')}}" alt="">
-                        </span>
-                        <div class="txt-holder ft-svg-child-2">
-                            <h1 class="f-promo-box-heading">GIFT CARDS</h1>
-                            <p>Give perfect gift</p>
-                        </div>
-                    </div>
-                    <!-- F Widget Item of the Page -->
-                </div>
-                <div class="col">
-                    <!-- F Widget Item of the Page -->
-                    <div class="f-widget-item ft-svg-class-4">
-                        <span class="widget-icon ft-svg-child-1">
-                            <img src="{{asset('frontend/images/products/footer-icon/payment.svg')}}" alt="">
-                        </span>
-                        <div class="txt-holder ft-svg-child-2">
-                            <h1 class="f-promo-box-heading">PAYMENT 100% SECURE</h1>
-                            <p>Payment 100% secure</p>
-                        </div>
-                    </div>
-                    <!-- F Widget Item of the Page -->
-                </div>
-            </div>
-        </div>
-    </aside>
-    <!-- F Promo Box of the Page end -->
-    <!-- Footer Holder of the Page -->
-    <div class="footer-holder dark">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-5 col-md-4 mt-paddingbottomxs">
-                    <!-- F Widget About of the Page -->
-                    <div class="f-widget-about">
-                        <div class="logo">
-                            <a href="frontend/#"><img src="frontend/images/channel-logo.svg" height="30" alt="Schon"></a>
-                        </div>
-                        <p>Exercitation ullamco laboris nisi ut aliquip ex<br> commodo consequat. Duis aute
-                            irure</p>
-                        <ul class="list-unstyled address-list">
-                            <li><i class="fa fa-map-marker"></i>
-                                <address>Connaugt Road Central Suite 18B, 148 <br>New Yankee</address>
-                            </li>
-                            <li><i class="fa fa-phone"></i><a href="frontend/tel:15553332211">+1 (555) 333
-                                    22 11</a>
-                            </li>
-                            <li><i class="fa fa-envelope-o"></i><a
-                                    href="frontend/mailto:info@schon.chair">&#105;&#110;&#102;&#111;&#064;&#115;&#099;&#104;&#111;&#110;&#046;&#099;&#104;&#097;&#105;&#114;</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- F Widget About of the Page -->
-                </div>
-                <nav class="col-xs-12 col-sm-7 col-md-5 mt-paddingbottomxs">
-                    <!-- Footer Nav of the Page -->
-                    <div class="nav-widget-1">
-                        <h3 class="f-widget-heading">Categories</h3>
-                        <ul class="list-unstyled f-widget-nav">
-                            <li><a href="">Skin</a></li>
-                            <li><a href="">Fragrances</a></li>
-                            <li><a href="">Haircare</a></li>
-                            <li><a href="">Personal Care</a></li>
-                            <li><a href="">Home Decore </a></li>
-                            <li><a href="">gift</a></li>
-                        </ul>
-                    </div>
-                    <!-- Footer Nav of the Page end -->
-                    <!-- Footer Nav of the Page -->
-                    <div class="nav-widget-1">
-                        <h3 class="f-widget-heading">Information</h3>
-                        <ul class="list-unstyled f-widget-nav">
-                            <li><a href="{{route('about')}}">FAQ</a></li>
-                            <li><a href="{{route('contact')}}">BLOG</a></li>
-                            <li><a href="">Support</a></li>
+                             <img src="{{ asset('frontend/images/products/footer-icon/free-shipping.svg') }}"
+                                 alt="">
 
+                         </span>
+                         <div class="txt-holder  ft-svg-child-2">
+                             <h1 class="f-promo-box-heading">FREE SHIPPING</h1>
+                             <p>Free shipping on all US order</p>
+                         </div>
+                     </div>
+                     <!-- F Widget Item of the Page end -->
+                 </div>
+                 <div class="col mt-paddingbottomsm ft-svg-class-2">
+                     <!-- F Widget Item of the Page -->
+                     <div class="f-widget-item ft-svg-sub-class-2">
+                         <span class="widget-icon ft-svg-child-1">
+                             <img src="{{ asset('frontend/images/products/footer-icon/support.svg') }}" alt="">
+                         </span>
+                         <div class="txt-holder ft-svg-child-2">
+                             <h1 class="f-promo-box-heading">SUPPORT 24/7</h1>
+                             <p>We support 24 hours a day</p>
+                         </div>
+                     </div>
+                     <!-- F Widget Item of the Page -->
+                 </div>
+                 <div class="col mt-paddingbottomxs ft-svg-class-3">
+                     <!-- F Widget Item of the Page -->
+                     <div class="f-widget-item ft-svg-sub-class-3">
+                         <span class="widget-icon ft-svg-child-1">
+                             <img src="{{ asset('frontend/images/products/footer-icon/gift-card.svg') }}"
+                                 alt="">
+                         </span>
+                         <div class="txt-holder ft-svg-child-2">
+                             <h1 class="f-promo-box-heading">GIFT CARDS</h1>
+                             <p>Give perfect gift</p>
+                         </div>
+                     </div>
+                     <!-- F Widget Item of the Page -->
+                 </div>
+                 <div class="col">
+                     <!-- F Widget Item of the Page -->
+                     <div class="f-widget-item ft-svg-class-4">
+                         <span class="widget-icon ft-svg-child-1">
+                             <img src="{{ asset('frontend/images/products/footer-icon/payment.svg') }}" alt="">
+                         </span>
+                         <div class="txt-holder ft-svg-child-2">
+                             <h1 class="f-promo-box-heading">PAYMENT 100% SECURE</h1>
+                             <p>Payment 100% secure</p>
+                         </div>
+                     </div>
+                     <!-- F Widget Item of the Page -->
+                 </div>
+             </div>
+         </div>
+     </aside>
+     <!-- F Promo Box of the Page end -->
+     <!-- Footer Holder of the Page -->
+     <div class="footer-holder dark">
+         <div class="container">
+             <div class="row">
+                 <div class="col-xs-12 col-sm-5 col-md-4 mt-paddingbottomxs">
+                     <!-- F Widget About of the Page -->
+                     <div class="f-widget-about">
+                         <div class="logo">
+                             <a href="frontend/#"><img src="frontend/images/channel-logo.svg" height="30"
+                                     alt="Schon"></a>
+                         </div>
+                         <p>Exercitation ullamco laboris nisi ut aliquip ex<br> commodo consequat. Duis aute
+                             irure</p>
+                        <ul class="list-unstyled d-flex gap-2 p-0">
+                             <li>
+                                <i class="fas fa-map-marker-alt text-red"></i>
+                             </li>
+                             <li>
+                                Exercitation ullamco laboris nisi ut aliquip excommodo consequat. Duis aute irure
+                             </li>
                         </ul>
-                    </div>
-                    <!-- Footer Nav of the Page end -->
-                    <!-- Footer Nav of the Page -->
-                    <div class="nav-widget-1">
-                        <h3 class="f-widget-heading">Company</h3>
-                        <ul class="list-unstyled f-widget-nav">
-                            <li><a href="">Abpit Us</a></li>
-                            <li><a href="">Careers</a></li>
-                            <li><a href="">Contact Us</a></li>
+                        <ul class="list-unstyled d-flex gap-2 p-0">
+                            <li>
+                                <i class="fas fa-phone  text-red"></i>
+                            </li>
+                            <li>
+                                Exercitation ullamco laboris
+                            </li>
+                       </ul>
+                       <ul class="list-unstyled d-flex gap-2 p-0">
+                        <li>
+                            <i class="fas fa-envelope  text-red"></i>
+                        </li>
+                        <li>
+                            Exercitation ullamco laboris
+                        </li>
+                       </ul>
 
-                        </ul>
-                    </div>
-                    <!-- Footer Nav of the Page end -->
-                </nav>
-                <div class="col-xs-12 col-md-3 text-right hidden-sm">
-                    <!-- F Widget Newsletter of the Page -->
-                    <div class="f-widget-newsletter">
-                        <h3 class="f-widget-heading">Subscribe</h3>
-                        <div class="holder">
-                            <form class="newsletter-form form2" action="#">
-                                <fieldset>
-                                    <input type="email" class="form-control" placeholder="Your e-mail">
-                                    <button type="submit">Subscribe</button>
-                                </fieldset>
-                            </form>
-                        </div>
-                      <p>
-                        Hello, we are Lift Media. Our goal is to translate the positive effects from revolutionizing how companies engage with their clients & their team.
-                      </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Footer Holder of the Page end -->
-    <!-- Footer Area of the Page -->
-    <div class="footer-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                    <p>© <a href="">schön.</a> - All rights Reserved</p>
-                </div>
-                <div class="col-xs-12 col-sm-6 text-right">
-                    <div class="bank-card">
-                        <img src="frontend/images/bank-card.png" alt="bank-card">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Footer Area of the Page end -->
-</footer>
-<!-- footer of the Page end -->
+
+                     </div>
+                     <!-- F Widget About of the Page -->
+                 </div>
+                 <nav class="col-xs-12 col-sm-7 col-md-5 mt-paddingbottomxs">
+                     <!-- Footer Nav of the Page -->
+                     <div class="nav-widget-1">
+                         <h3 class="f-widget-heading text-red">Categories</h3>
+                         <ul class="list-unstyled f-widget-nav">
+                             <li><a href="">Skin</a></li>
+                             <li><a href="">Fragrances</a></li>
+                             <li><a href="">Haircare</a></li>
+                             <li><a href="">Personal Care</a></li>
+                             <li><a href="">Home Decore </a></li>
+                             <li><a href="">gift</a></li>
+                         </ul>
+                     </div>
+                     <!-- Footer Nav of the Page end -->
+                     <!-- Footer Nav of the Page -->
+                     <div class="nav-widget-1">
+                         <h3 class="f-widget-heading">Information</h3>
+                         <ul class="list-unstyled f-widget-nav">
+                             <li><a href="{{ route('about') }}">FAQ</a></li>
+                             <li><a href="{{ route('contact') }}">BLOG</a></li>
+                             <li><a href="">Support</a></li>
+
+                         </ul>
+                     </div>
+                     <!-- Footer Nav of the Page end -->
+                     <!-- Footer Nav of the Page -->
+                     <div class="nav-widget-1">
+                         <h3 class="f-widget-heading">Company</h3>
+                         <ul class="list-unstyled f-widget-nav">
+                             <li><a href="">Abpit Us</a></li>
+                             <li><a href="">Careers</a></li>
+                             <li><a href="">Contact Us</a></li>
+
+                         </ul>
+                     </div>
+                     <!-- Footer Nav of the Page end -->
+                 </nav>
+                 <div class="col-xs-12 col-md-3 text-right">
+                     <!-- F Widget Newsletter of the Page -->
+                     <div class="f-widget-newsletter f-widget-news-custome">
+                         <h3 class="f-widget-heading" style="line-height:0px">Subscribe</h3>
+                         <div class="holder">
+                             <form class="newsletter-form form2" action="#">
+                                 <fieldset>
+                                     <input type="email" class="form-control" placeholder="Your e-mail" style="border-radius:0px">
+                                     <button type="submit">
+                                        <i class="fas fa-arrow-right"></i>
+                                     </button>
+                                 </fieldset>
+                             </form>
+                         </div>
+                         <p class="text-red">
+                             Hello, we are Lift Media. Our goal is to translate the positive effects from
+                             revolutionizing how companies engage with their clients & their team.
+                         </p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <!-- Footer Holder of the Page end -->
+     <!-- Footer Area of the Page -->
+     <div class="footer-area">
+         <div class="container">
+             <div class="row">
+                 <div class="col-xs-12 col-sm-6">
+                     <p> © 2023 channel. All Rights Reserved.</p>
+                 </div>
+                 <div class="col-xs-12 col-sm-6 text-right">
+                     <div class="bank-card">
+                         Designed & Developed by Acetrot
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <!-- Footer Area of the Page end -->
+ </footer>
+ <!-- footer of the Page end -->
