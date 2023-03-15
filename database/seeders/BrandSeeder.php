42917109d91fea7e6c6e3a70398e16702a47eca3
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2023-03-15 13:10:42 +0530

    brand seeder add

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class BrandSeeder extends Seeder
 {
@@ -13,6 +16,23 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $name = [
+            'Brand 1',
+            'Brand 2',
+            'Brand 3',
+            'Brand 4',
+            'Brand 5',
+        ];
+        $faker = Faker::create();
+        foreach ($name as $n) {
+            DB::table('brands')->insert([
+                "name" => $n,
+                "slug" => Str::slug($n),
+                "image" => $faker->randomElement([ 'image1.png','image2.png', 'image3.png', 'image4.png', 'image5.png',]),
+                "descriptions" => $faker->paragraph,
+                "created_at" => now(),
+                "updated_at" => now()
+            ]);
+        }
     }
 }
