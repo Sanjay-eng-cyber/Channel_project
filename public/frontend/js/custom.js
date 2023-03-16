1a2c858bb2bb9ab8d4d6bb92fedb83502db120ee
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-16 13:34:40 +0530

    breakpoint set in second slider

--- public/frontend/js/custom.js
+++ public/frontend/js/custom.js
@@ -56,27 +56,22 @@ jQuery('.fragrances').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
-
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-            breakpoint: 768,
+            breakpoint: 1200,
             settings: {
               slidesToShow: 2,
             }
           },
+
         {
-          breakpoint: 599,
-          settings: {
-            slidesToShow: 2,
-          }
-        },
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+
         {
-          breakpoint: 479,
+          breakpoint: 320,
           settings: {
             slidesToShow: 1,
           }
