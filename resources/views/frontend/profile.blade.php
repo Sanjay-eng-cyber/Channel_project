d1e0876526fccf4771e960bd7258a4c1eee07017
Kishan-Yadav-Acetrot <98552345+Acetrot-Kishan@users.noreply.github.com>
2023-03-18 18:42:00 +0530

    profile css separated

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -1,5 +1,8 @@
 @extends('frontend.layouts.app')
 @section('title', 'Profile')
+@section('cdn')
+    <link rel="stylesheet" href="{{ url('frontend/css/profile.css') }}">
+@endsection
 @section('content')
     <section class="profile-breadcrumb">
         <div class="bg-stipe"></div>
@@ -123,12 +126,13 @@
                         </div>
                         <div class="row">
                             <div class="col-sm-6 py-2">
-                           <img src="frontend/images/organic-product/change-p-pic.png"  class="img-fluid" alt="" style="width: 255px;"
-                          >
+                                <img src="frontend/images/organic-product/change-p-pic.png" class="img-fluid"
+                                    alt="" style="width: 255px;">
                             </div>
 
                             <div class="col-sm-6 py-2">
-                                <input type="text" class=" profile-form-input-custome" placeholder="Confirm New Password">
+                                <input type="text" class=" profile-form-input-custome"
+                                    placeholder="Confirm New Password">
                             </div>
 
                         </div>
@@ -144,39 +148,42 @@
                 <div class="col-12 col-lg-4 py-5 py-lg-0 py-xl-0 py-xxl-0">
 
                     <div class="profile-form-border p-4">
-                           <div class="row display: flex; align-items: center">
+                        <div class="row display: flex; align-items: center">
 
-                                    <div class="col-6 py-2">
-                                        <h5 class="main-head text-red">Address 1</h5>
-                                    </div>
+                            <div class="col-6 py-2">
+                                <h5 class="main-head text-red">Address 1</h5>
+                            </div>
 
-                                    <div class="col-6 py-2">
-                                        <ul class="d-flex gap-4 list-unstyled">
-                                            <li>
-                                                <button type="button" class="btn btn-primary position-relative profile-s-bg-color">
-                                                   Home
-                                                    <span class="position-absolute top-0 start-100 translate-middle  bg-success border border-light rounded-circle profile-alert-icon" >
-                                                    <i class="fas fa-check" style="color:white"></i>
-                                                    <span class="visually-hidden">New alerts</span>
-                                                    </span>
-                                                </button>
+                            <div class="col-6 py-2">
+                                <ul class="d-flex gap-4 list-unstyled">
+                                    <li>
+                                        <button type="button"
+                                            class="btn btn-primary position-relative profile-s-bg-color">
+                                            Home
+                                            <span
+                                                class="position-absolute top-0 start-100 translate-middle  bg-success border border-light rounded-circle profile-alert-icon">
+                                                <i class="fas fa-check" style="color:white"></i>
+                                                <span class="visually-hidden">New alerts</span>
+                                            </span>
+                                        </button>
 
-                                            </li>
-                                            <li>
-                                                <i class="far fa-trash-alt fa-1x profile-trash-icon" ></i>
-                                                <!-- alternate trash icon with red background color and circle border radius -->
-                                            </li>
+                                    </li>
+                                    <li>
+                                        <i class="far fa-trash-alt fa-1x profile-trash-icon"></i>
+                                        <!-- alternate trash icon with red background color and circle border radius -->
+                                    </li>
 
-                                        </ul>
-                                    </div>
+                                </ul>
+                            </div>
 
-                                    <div class="col-12">
-                                        <h6 class="main-head">User name</h6>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facere quaerat eaque pariatur aliquam a expedita illum aliquid cumque quae!
-                                        </p>
-                                    </div>
-                           </div>
+                            <div class="col-12">
+                                <h6 class="main-head">User name</h6>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facere quaerat eaque
+                                    pariatur aliquam a expedita illum aliquid cumque quae!
+                                </p>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="py-3"></div>
@@ -240,10 +247,11 @@
 
                                 </div>
                                 <div class="col-sm-6 py-2 profile-form-label-color">
-                                    <select class="form-select-lg mb-3 profile-form-input-custome profile-f-l-color" aria-label=".form-select-lg example">
-                                    <option selected class="profile-f-l-color border-0">Home</option>
-                                    <option value="1" class="profile-f-l-color">Office</option>
-                                    <option value="2" class="profile-f-l-color">Other</option>
+                                    <select class="form-select-lg mb-3 profile-form-input-custome profile-f-l-color"
+                                        aria-label=".form-select-lg example">
+                                        <option selected class="profile-f-l-color border-0">Home</option>
+                                        <option value="1" class="profile-f-l-color">Office</option>
+                                        <option value="2" class="profile-f-l-color">Other</option>
                                     </select>
                                 </div>
                             </div>
@@ -270,211 +278,4 @@
 
 
     </section>
-
-    {{-- we will move this styles in css file before production --}}
-
-    <style>
-.profile-s-bg-color{
-    background-color: rgba(236, 38, 143, 0.4);
-    color: black;
-    border: 0.782313px solid rgba(236, 38, 143, 0.4);
-border-radius: 637.585px;
-}
-.profile-s-bg-color:hover{
-    background-color: rgba(236, 38, 143, 0.4);
-    color: black;
-    border: 0.782313px solid rgba(236, 38, 143, 0.4);
-
-}
-.profile-trash-icon{
-  background-color: rgba(236, 38, 143, 0.4); border-radius: 50%; padding: 10px; width:15px;height:15px;
-}
-.profile-alert-icon{
-    border-radius: 50%;
-    height:24px;width:24px;
-}
- .profile-btn-color {
-            background: rgba(236, 38, 143, 1);
-            color: #fff;
-
-        }
-.profile-btn-color:hover {
-    color:#FFFFFF;
-    background-color:rgba(236, 38, 143, 1);;
-    border-color: rgba(236, 38, 143, 1);;
-}
-
-        .profile-f-l-color {
-            color: rgba(57, 53, 53, 0.34);
-        }
-
-        .profile-form-border {
-            border: 0.798412px solid #000000;
-            border-radius: 11.9762px;
-        }
-
-        .profile-form-group-star-name {
-            position: relative;
-        }
-
-        .profile-form-group-star-name::after {
-            content: '*';
-            position: absolute;
-            top: 14px;
-            left: 50px;
-            color: rgba(241, 0, 0, 0.7);
-        }
-
-        .profile-form-group-star-pin-code {
-            position: relative;
-        }
-
-        .profile-form-group-star-pin-code::after {
-            content: '*';
-            position: absolute;
-            top: 14px;
-            left: 72px;
-            color: rgba(241, 0, 0, 0.7);
-        }
-
-
-        .profile-form-input-custome {
-            display: block;
-            width: 100%;
-            padding: 0.5rem 0rem;
-            font-size: 1rem;
-            font-weight: 400;
-            line-height: 1.5;
-            border-top: none;
-            border-right: none;
-            border-left: none;
-            border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-        }
-
-        .profile-form-input-custome::placeholder {
-            text-align: left;
-            color: rgba(57, 53, 53, 0.34);
-        }
-
-        .profile-form-input-custome:focus {
-            outline: none;
-            border-left: none;
-            border-right: none;
-            border-top: none;
-            border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-        }
-
-
-        .checkbox-custome-p {}
-
-        .profile-breadcrumb {
-            position: relative;
-            margin-top: 1em;
-            margin-bottom: 1em;
-        }
-
-        .profile-breadcrumb .container {
-            z-index: 9;
-            position: relative;
-        }
-
-        .profile-breadcrumb .bg-stipe {
-            background: #EC268F99;
-            position: absolute;
-            width: 100%;
-            height: 70%;
-            top: 30%;
-            left: 0;
-            -webkit-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            z-index: 0;
-
-        }
-
-
-        .profile-photo-section {
-            max-width: 250px;
-            position: relative;
-            margin: auto;
-        }
-
-        .profile-photo {
-            border: 5px solid #EC268F;
-            border-radius: 30em
-        }
-
-        .edit-photo-btn {
-            position: absolute;
-            bottom: 1em;
-            right: 1em;
-            width: 4em;
-            height: 4em;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: .3em;
-            border-radius: 5em;
-            background: #D9D9D9;
-        }
-
-        .edit-photo-btn:hover {
-            background: #fff;
-        }
-
-        .profile {
-            margin-bottom: 1em
-        }
-
-        .user-name-section {
-            max-width: 45ch;
-            margin: auto;
-        }
-
-        .name-input {
-            background: none;
-            border: none;
-            border-bottom: 4px solid rgba(0, 0, 0, 0.1);
-            color: white;
-            font-weight: 500;
-            font-size: 28px;
-            text-align: center;
-        }
-
-        .profile-menu {
-            display: flex;
-            justify-content: center;
-            align-items: flex-end;
-            flex-wrap: wrap;
-            background: #FFFFFF;
-            box-shadow: 8px 8px 12px rgba(0, 0, 0, 0.15);
-            border-radius: 15px;
-            padding-top: 1em;
-            padding-bottom: 1em;
-        }
-
-        .profile-menu a.btn {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            font-size: 15px;
-            border: none;
-        }
-
-        .menu-icon {
-            width: 5em;
-            height: 5em;
-            border-radius: 10em;
-            padding: 1.2em;
-            margin-bottom: 0.2em;
-            background: #FFFFFF;
-            box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.52);
-            transition: box-shadow 1s;
-        }
-
-        .profile-menu a.btn:hover .menu-icon {
-            box-shadow: -2px -8px 16px 0px rgba(0, 0, 0, 0.52);
-        }
-    </style>
 @endsection
