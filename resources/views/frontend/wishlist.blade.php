586490d254da7955566ac88b6b3a0f018d67309f
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-18 19:22:04 +0530

    heading & search input add in wishlist

--- resources/views/frontend/wishlist.blade.php
+++ resources/views/frontend/wishlist.blade.php
@@ -65,4 +65,20 @@
 </section>
 
 
+<section>
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <h4 class="main-head text-red text-center pt-4"> Wishes Do Come True </h4>
+        <div class="col-sm-9">
+            <div class="input-group py-3">
+                <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <span class="input-group-text" id="basic-addon2">
+                    <i class="fas fa-search fa-fw text-red"></i>
+                </span>
+              </div>
+        </div>
+    </div>
+</div>
+</section>
+
 @endsection
