d851f1a9f678af692774ccf8ba48890ed67865dc
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-18 14:50:27 +0530

    profile top form done

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -56,12 +56,257 @@
 
             </div>
         </div>
+
+
     </section>
 
+    <section>
+        <div class="container">
+            <div class="row py-5 d-flex justify-content-center">
+                <div class="col-12 col-lg-6 ">
+                    <form class="p-5 profile-form-border" action="" method="post">
+                        <h5 class="main-head text-red">Personal Information</h5>
+                        <div class="row">
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="First Name">
+                            </div>
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Mobile">
+                            </div>
+
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Email">
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+
+                            <div class="col-sm-6 py-2">
+                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
+                                    value="option1" checked placeholder="male">
+                                <label class="form-check-label profile-f-l-color" for="exampleRadios1">
+                                    Male
+                                </label>
+                            </div>
+
+
+                            <div class="col-sm-6 py-2">
+                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
+                                    value="option1" checked>
+                                <label class="form-check-label profile-f-l-color" for="exampleRadios1">
+                                    Female
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 pt-4 text-end">
+                            <button type="submit" class="btn profile-btn-color">Update profile</button>
+                        </div>
+
+                    </form>
+                    <div class="py-4">
+                    </div>
+                    <form class="p-5 profile-form-border" action="" method="post">
+                        <h5 class="main-head text-red">Change Password</h5>
+                        <div class="row">
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Current Password">
+                            </div>
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="New Password">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 py-2">
+                           <img src="frontend/images/organic-product/change-p-pic.png"  class="img-fluid" alt="" style="width: 255px;"
+                          >
+                            </div>
+
+                            <div class="col-sm-6 py-2">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Confirm New Password">
+                            </div>
+
+                        </div>
+
+                        <div class="col-sm-12 pt-4 pt-lg-0  text-end">
+                            <button type="submit" class="btn profile-btn-color">Update password</button>
+                        </div>
+
+
+                    </form>
+                </div>
+
+
+
+                <div class="col-12 col-lg-4 py-5 py-lg-0 py-xl-0 py-xxl-0">
+                    <form class="p-5 profile-form-border ">
+
+                        <h5 class="main-head text-red">Address</h5>
+
+                        <div class="form-group profile-form-group-star-name py-2">
+                            <input type="text" class="profile-form-input-custome" placeholder="Name*">
+
+                        </div>
+
+
+                        <div class="form-group  profile-form-group-star-pin-code py-2">
+                            <input type="text" class="profile-form-input-custome" placeholder="Pin Code*">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="Enter name">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="Address*">
+                        </div>
+
+                        <div class="pt-5 pb-3">
+                            <hr class="m-0">
+                        </div>
+
+
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="Landmark">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="City">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="State">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="Country">
+                        </div>
+
+                        <div class="form-group py-2">
+                            <input type="text" class=" profile-form-input-custome" placeholder="Mobile No*">
+
+                        </div>
+
+
+
+                        <div class="row">
+                            <div class="col-sm-6 py-2">
+
+                                <label for="" class="profile-f-l-color"> Address Type</label>
+
+                            </div>
+                            <div class="col-sm-6 py-2 profile-form-label-color">
+                                <select class="form-select-lg mb-3 profile-form-input-custome profile-f-l-color profile-box-s" aria-label=".form-select-lg example">
+                                  <option selected class="profile-f-l-color border-0">Home</option>
+                                  <option value="1" class="profile-f-l-color">Office</option>
+                                  <option value="2" class="profile-f-l-color">Other</option>
+                                </select>
+                              </div>
+
+
+
+                        </div>
+
+
+                        <div class="col-sm-12 pt-4">
+                            <button type="submit" class="btn profile-btn-color">Save</button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
 
     {{-- we will move this styles in css file before production --}}
 
     <style>
+
+
+  .profile-box-s {
+        font-size: 18px;
+        color: #1c87c9;
+        background-color: #FFFFFF;
+        border-radius: 5px;
+        box-shadow: 0px 3.19365px 3.19365px rgba(0, 0, 0, 0.25);
+      }
+        .profile-btn-color {
+            background: rgba(236, 38, 143, 1);
+            color: #fff;
+
+        }
+
+        .profile-f-l-color {
+            color: rgba(57, 53, 53, 0.34);
+        }
+
+        .profile-form-border {
+            border: 0.798412px solid #000000;
+            border-radius: 11.9762px;
+        }
+
+        .profile-form-group-star-name {
+            position: relative;
+        }
+
+        .profile-form-group-star-name::after {
+            content: '*';
+            position: absolute;
+            top: 14px;
+            left: 50px;
+            color: rgba(241, 0, 0, 0.7);
+        }
+
+        .profile-form-group-star-pin-code {
+            position: relative;
+        }
+
+        .profile-form-group-star-pin-code::after {
+            content: '*';
+            position: absolute;
+            top: 14px;
+            left: 72px;
+            color: rgba(241, 0, 0, 0.7);
+        }
+
+
+        .profile-form-input-custome {
+            display: block;
+            width: 100%;
+            padding: 0.5rem 0rem;
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            border-top: none;
+            border-right: none;
+            border-left: none;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+        }
+
+        .profile-form-input-custome::placeholder {
+            text-align: left;
+            color: rgba(57, 53, 53, 0.34);
+        }
+
+        .profile-form-input-custome:focus {
+            outline: none;
+            border-left: none;
+            border-right: none;
+            border-top: none;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+        }
+
+
+        .checkbox-custome-p {}
+
         .profile-breadcrumb {
             position: relative;
             margin-top: 1em;
