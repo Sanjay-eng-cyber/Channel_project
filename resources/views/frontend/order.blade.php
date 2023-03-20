29712e8a048a4a6d98afcb128cb52f4904d8de97
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-20 18:26:37 +0530

    along with search btn top created in order page

--- resources/views/frontend/order.blade.php
+++ resources/views/frontend/order.blade.php
@@ -64,6 +64,45 @@
 
     </section>
 
+    <section style="padding:30px 0px 50px 0px">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
 
+
+                <div class="col-lg-9 col-xl-3 col-xxl-4">
+                    <div class="input-group py-3">
+                        <input type="text" class="form-control" placeholder="" aria-label="Recipient's username"
+                            aria-describedby="basic-addon2">
+                        <span class="input-group-text" id="basic-addon2" style="background-color: rgba(236, 38, 143, 0.4);">
+                            <i class="fas fa-search fa-fw text-red"></i>
+                        </span>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-9 col-xl-6 col-xxl-5 order-main">
+                        <ul class="list-unstyled order-main-li-section" >
+                        <li class="cmc wishlist-order" style="border-bottom: 4px solid rgba(0, 175, 239, 1);">
+                            My Order
+                        </li>
+                        <li class="cmc wishlist-ship">
+                            Not Yet Shipped
+                        </li>
+                        <li class="cmc wishlist-buy">
+                            Buy Again
+                        </li>
+                        <li class="cmc can-ord">
+                            Cancelled Orders
+                        </li>
+                        </ul>
+                </div>
+
+
+
+
+            </div>
+        </div>
+    </section>
 @endsection
 
