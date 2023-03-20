9db68b339619b6c91b8a3ccf8000c4100c65c2cf
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-20 19:29:32 +0530

    1st card added along with img,btn,text

--- resources/views/frontend/order.blade.php
+++ resources/views/frontend/order.blade.php
@@ -68,7 +68,6 @@
         <div class="container">
             <div class="row d-flex justify-content-center">
 
-
                 <div class="col-lg-9 col-xl-3 col-xxl-4">
                     <div class="input-group py-3">
                         <input type="text" class="form-control" placeholder="" aria-label="Recipient's username"
@@ -78,8 +77,6 @@
                         </span>
                     </div>
                 </div>
-
-
 
                 <div class="col-lg-9 col-xl-6 col-xxl-5 order-main">
                         <ul class="list-unstyled order-main-li-section" >
@@ -98,10 +95,55 @@
                         </ul>
                 </div>
 
-
-
-
             </div>
+
+            <div class="row py-4">
+                <div class="d-flex justify-content-center my-order-main">
+                    <div class="col-sm-8  my-order-main-out">
+                        <div class="row  my-order-main-in">
+                            <div class="col-sm-12 col-md-4 col-lg-4 col-xl-3 d-flex justify-content-center flex-column my-order-main-in-img">
+                                <img src="frontend/images/products/skin/sk1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="col-sm-12  col-md-8 col-lg-8 col-xl-6  my-order-main-desc">
+
+                                <h5 class="main-head">Essence Long Lasting Eye Pencil</h5>
+                                <p style="font-size: 14px;opacity: 0.6;">essence Long Lasting Eye Pencil is a creamy and
+                                    pigmented eye pencil that brightens and accentuates your eye more....</p>
+
+                                <ul class="list-unstyled d-flex gap-3">
+                                    <li class="price" >From ₹ 145.55</li>
+                                    <li class="status">In Stock</li>
+                                </ul>
+
+                            </div>
+                            <div class="col-sm-12  col-md-12 col-lg-12 col-xl-3 py-3 py-xl-0 py-xxl-0  my-order-main-in-btn gap-2"
+                            style="
+                                 display: flex;
+    flex-direction: column;
+    justify-content: center;
+                            "
+                            >
+
+                                <a href="http://" style="font-size:15px">Arriving Wednesday</a>
+                                <button type="submit" class="btn  add-pro-btn">Track Order</button>
+                                <button type="submit" class="btn  add-pro-btn">Order Details</button>
+
+
+
+                                    {{-- <a href="" style="font-size:12px" class="py-1 text-red">
+                                        Remove item from Wishlist
+                                    </a> --}}
+
+                                {{-- <a href="http://">Remove item from Wishlist</a> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
     </section>
 @endsection
