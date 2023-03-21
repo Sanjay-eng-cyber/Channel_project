3006a00daab53849e78fdcd2065f38058c21e94e
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-21 16:26:23 +0530

    order second card done

--- resources/views/frontend/order.blade.php
+++ resources/views/frontend/order.blade.php
@@ -124,7 +124,7 @@
                                     <li class="status">
                                         <ul class="gap-2 d-flex flex-row flex-lg-column flex-xl-column  gap-lg-2 justify-content-between" style="padding: 0px 0px 0px 17px;">
                                             <li class="return-order">Return Order</li>
-                                            <li class="cancel-order">Cancel Order</li>
+                                            <li class="cancel-order text-red">Cancel Order</li>
                                         </ul>
                                     </li>
                                     <li class="price">From ₹ 145.55</li>
@@ -142,9 +142,48 @@
                 </div>
             </div>
 
+            <div class="row py-4">
+                <div class="d-flex justify-content-center my-order-main">
+                    <div class="col-sm-10  my-order-main-out">
+                        <div class="row  my-order-main-in">
+                            <div class="col-sm-12 col-md-4 col-lg-4 col-xl-3 d-flex justify-content-center flex-column my-order-main-in-img">
+                                <img src="frontend/images/products/skin/sk1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="col-sm-12  col-md-8 col-lg-8 col-xl-6  my-order-main-desc">
+
+                                <h5 class="main-head">Essence Long Lasting Eye Pencil</h5>
+                                <p>essence Long Lasting Eye Pencil is a creamy and
+                                    pigmented eye pencil that brightens and accentuates your eye more....</p>
+
+                                <ul class="list-unstyled d-flex gap-2 flex-column flex-lg-row">
+                                   <li>
+                                    <ul class="gap-2 d-flex flex-row flex-lg-column flex-xl-column  gap-lg-1 justify-content-between" style="padding: 0px 0px 0px 17px;">
+                                        <li>Expected Delivery</li>
+                                        <li>19 March 2023</li>
+                                    </ul>
 
 
+                                   </li>
+                                    <li class="status">
+                                        <ul class="gap-2 d-flex flex-row flex-lg-column flex-xl-column  gap-lg-2 justify-content-between" style="padding: 0px 0px 0px 17px;">
+                                            <li class="return-order">Return Order</li>
+                                            <li class="cancel-order text-red">Cancel Order</li>
+                                        </ul>
+                                    </li>
+                                    <li class="price">From ₹ 145.55</li>
 
+                                </ul>
+
+                            </div>
+                            <div class="col-sm-12  col-md-12 col-lg-12 col-xl-3 py-3 py-xl-0 py-xxl-0  my-order-main-in-btn gap-2">
+                                <a href="http://" class="arriving-2-or-btn">Arriving Friday</a>
+                                <a href="http://" class=" opacity-70">Preparing for Dispatch</a>
+                                <button type="submit" class="btn  add-orderDetails-btn">Order Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
         </div>
