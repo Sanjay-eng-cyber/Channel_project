a677671e337741116dc84692021f72d162df4035
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-23 16:32:25 +0530

    retrun order second page second card done

--- resources/views/frontend/return-order-second.blade.php
+++ resources/views/frontend/return-order-second.blade.php
@@ -5,7 +5,41 @@
 @endsection
 @section('content')
     <section style="padding: 100px 0px 100px 0px">
-     
+        <div class="container">
+            <div class="row d-flex justify-content-center gap-3">
+                <div class="col-lg-12 col-xl-5">
+
+                </div>
+
+                <div class="col-lg-12 col-xl-6 or-secondpage-scard">
+                    <div class="p-4 or-secondpage-scard">
+
+                            <h5 class="main-head py-3 or-secondpage-scard-fhead">Recommended based on your purchase</h5>
+                            <div class="row pt-3 pb-3 or-secondpage-scard-card">
+                                <div class="col-sm-4 or-secondpage-scard-card-img" style="">
+                                    <img src="frontend/images/products/skin/sk1.png" alt="" class="img-fluid">
+                                </div>
+                                <div class="col-sm-8 or-secondpage-scard-card-des pt-4 pt-sm-0 pt-md-0">
+                                    <h4 class="main-head">Essence Long Lasting Eye Pencil</h4>
+                                    <p>
+                                        essence Long Lasting Eye Pencil is a creamy and pigmented eye pencil that brightens
+                                        and
+                                        accentuates your eye more....
+                                    </p>
+                                    <ul class="d-flex gap-5 p-0">
+                                        <li class="no-bullet fw-bolder">
+                                            From ₹145.55
+                                        </li>
+                                        <li>13 March 2023</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <button type="button" class="btn bg-orange or-secondpage-lbtn mt-4">Continue Shopping</button>
+                    </div>
+                </div>
+            </div>
+       </div>
     </section>
 @endsection
 
