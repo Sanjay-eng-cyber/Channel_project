6fd9788d63c86c8f44ce17312720a57998f2e80c
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-24 15:43:20 +0530

    cancel order second page created

---
+++ resources/views/frontend/cancel-order-second.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.layouts.app')
+@section('title', 'Cancel Orders Second |')
+@section('cdn')
+    <link rel="stylesheet" href="{{ url('frontend/css/profile.css') }}">
+@endsection
+@section('content')
+
+
+
+@endsection
