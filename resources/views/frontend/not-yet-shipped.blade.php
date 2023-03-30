6b19a597a2e507ac6dffdd74a1690c7a020cd282
Kishan-Yadav-Acetrot <98552345+Acetrot-Kishan@users.noreply.github.com>
2023-03-30 16:25:56 +0530

    profile component dynamic on all page

--- resources/views/frontend/not-yet-shipped.blade.php
+++ resources/views/frontend/not-yet-shipped.blade.php
@@ -5,65 +5,7 @@
 @endsection
 @section('content')
 
-<section class="profile-breadcrumb">
-    <div class="bg-stipe"></div>
-    <div class="container">
-
-        <div class="profile">
-            <div class="profile-photo-section">
-                <img src="https://via.placeholder.com/300" class="profile-photo" alt="">
-                <button class="btn edit-photo-btn">
-                    <img src="{{ url('frontend/images/icons/icon-camera.svg') }}" alt="">
-                </button>
-            </div>
-            <form action="" class="user-name-section">
-                <div class="input-group">
-                    <input type="text" class="form-control name-input" value="User name">
-                    <button class="btn input-group-append">
-                        <img src="{{ url('frontend/images/icons/icon-edit.svg') }}" alt="">
-                    </button>
-                </div>
-            </form>
-        </div>
-        <div class="profile-menu">
-
-            <a href="javascript:;" class="btn">
-                <span class="menu-icon">
-                    <img src="{{ url('frontend/images/icons/icon-user.svg') }}" alt="">
-                </span>
-                My Profile
-            </a>
-            <a href="javascript:;" class="btn">
-                <span class="menu-icon">
-                    <img src="{{ url('frontend/images/icons/icon-user.svg') }}" alt="">
-                </span>
-                My Wishlist
-            </a>
-            <a href="javascript:;" class="btn">
-                <span class="menu-icon">
-                    <img src="{{ url('frontend/images/icons/icon-user.svg') }}" alt="">
-                </span>
-                My Orders
-            </a>
-            <a href="javascript:;" class="btn">
-                <span class="menu-icon">
-                    <img src="{{ url('frontend/images/icons/icon-user.svg') }}" alt="">
-                </span>
-                Reviews
-            </a>
-            <a href="javascript:;" class="btn">
-                <span class="menu-icon">
-                    <img src="{{ url('frontend/images/icons/icon-user.svg') }}" alt="">
-                </span>
-                Payment
-            </a>
-
-        </div>
-    </div>
-
-
-</section>
-
+<x-frontend.profile-nav image="https://via.placeholder.com/300" name="users name" />
 
 <section style="padding:30px 0px 50px 0px">
     <div class="container">
