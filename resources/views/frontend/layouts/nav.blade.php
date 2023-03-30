e4ed30a6b7aeed89a4efa7637e0865687a9d6e40
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-03-30 20:35:22 +0530

    nav updated

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -113,7 +113,7 @@
                        <nav id="nav" class="navbar hide-navbar">
                            <ul style="margin-right: 0px">
                                <li><a href="{{ route('index') }}">Home</a></li>
-                               <li><a href="{{ route('products') }}">Skin</a></li>
+                               <li><a href="{{ route('skin') }}">Skin</a></li>
                                <li><a href="{{ route('about') }}">fragrances</a></li>
                                <li><a href="{{ route('about') }}">Haircare</a></li>
                                <li><a href="{{ route('about') }}">PersonalCare</a></li>
