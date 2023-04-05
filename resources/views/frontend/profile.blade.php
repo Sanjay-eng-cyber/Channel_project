e03cee37f33fe75200c77d24c3d158ded1515904
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2023-04-05 18:13:43 +0530

    address input fixes

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -17,34 +17,37 @@
                         <div class="row">
                             <div class="col-sm-6 py-2">
                                 <input type="text" name="first_name" class=" profile-form-input-custome"
-                                    placeholder="First Name" minlength="3" maxlength="30" value="{{ old('first_name') ?? $user->first_name }}" required>
-                                    @if ($errors->has('first_name'))
-                                            <div id="first_name-error" class="text-primary">{{ $errors->first('first_name') }}</div>
-                                        @endif
+                                    placeholder="First Name" minlength="3" maxlength="30"
+                                    value="{{ old('first_name') ?? $user->first_name }}" required>
+                                @if ($errors->has('first_name'))
+                                    <div id="first_name-error" class="text-primary">{{ $errors->first('first_name') }}</div>
+                                @endif
                             </div>
                             <div class="col-sm-6 py-2">
                                 <input type="text" name="last_name" class=" profile-form-input-custome"
-                                    placeholder="Last Name" minlength="3" maxlength="30" required value="{{old('last_name') ?? $user->last_name}}">
-                                    @if ($errors->has('last_name'))
-                                            <div id="last_name-error" class="text-primary">{{ $errors->first('last_name') }}</div>
-                                        @endif
+                                    placeholder="Last Name" minlength="3" maxlength="30" required
+                                    value="{{ old('last_name') ?? $user->last_name }}">
+                                @if ($errors->has('last_name'))
+                                    <div id="last_name-error" class="text-primary">{{ $errors->first('last_name') }}</div>
+                                @endif
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-6 py-2">
                                 <input type="text" name="phone" class=" profile-form-input-custome"
-                                    placeholder="Mobile" required minlength="10" maxlength="10" value="{{old('phone') ?? $user->phone}}" disabled>
-                                    @if ($errors->has('phone'))
-                                            <div id="phone-error" class="text-primary">{{ $errors->first('phone') }}</div>
-                                        @endif
+                                    placeholder="Mobile" required minlength="10" maxlength="10"
+                                    value="{{ old('phone') ?? $user->phone }}" disabled>
+                                @if ($errors->has('phone'))
+                                    <div id="phone-error" class="text-primary">{{ $errors->first('phone') }}</div>
+                                @endif
                             </div>
 
                             <div class="col-sm-6 py-2">
-                                <input type="text" name="email" class=" profile-form-input-custome"
-                                    placeholder="Email" minlength="5" maxlength="40" required value="{{old('email') ?? $user->email}}">
-                                    @if ($errors->has('email'))
-                                            <div id="email-error" class="text-primary">{{ $errors->first('email') }}</div>
-                                        @endif
+                                <input type="text" name="email" class=" profile-form-input-custome" placeholder="Email"
+                                    minlength="5" maxlength="40" required value="{{ old('email') ?? $user->email }}">
+                                @if ($errors->has('email'))
+                                    <div id="email-error" class="text-primary">{{ $errors->first('email') }}</div>
+                                @endif
                             </div>
 
                         </div>
@@ -53,7 +56,8 @@
 
                             <div class="col-sm-6 py-2">
                                 <input class="form-check-input" type="radio" name="gender" id="exampleRadios1"
-                                    value="male" @if($user->gender == 'male') {{'checked'}} @endif placeholder="male" required>
+                                    value="male" @if ($user->gender == 'male') {{ 'checked' }} @endif
+                                    placeholder="male" required>
                                 <label class="form-check-label profile-f-l-color" for="exampleRadios1">
                                     Male
                                 </label>
@@ -62,14 +66,14 @@
 
                             <div class="col-sm-6 py-2">
                                 <input class="form-check-input" type="radio" name="gender" id="exampleRadios1"
-                                    value="female" @if ($user->gender == 'female') {{'checked'}} @endif required>
+                                    value="female" @if ($user->gender == 'female') {{ 'checked' }} @endif required>
                                 <label class="form-check-label profile-f-l-color" for="exampleRadios1">
                                     Female
                                 </label>
                             </div>
                             @if ($errors->has('gender'))
-                                            <div id="gender-error" class="text-primary">{{ $errors->first('gender') }}</div>
-                                        @endif
+                                <div id="gender-error" class="text-primary">{{ $errors->first('gender') }}</div>
+                            @endif
                         </div>
                         <div class="col-sm-12 pt-4 text-end">
                             <button type="submit" class="btn profile-btn-color">Update profile</button>
@@ -154,34 +158,10 @@
                     <div class="p-4 profile-form-border ">
                         <form class="">
 
-                            <h5 class="main-head text-red">Address 2</h5>
-
-                            <div class="form-group profile-form-group-star-name py-2">
-                                <input type="text" class="profile-form-input-custome" placeholder="Name*">
-
-                            </div>
-
-
-                            <div class="form-group  profile-form-group-star-pin-code py-2">
-                                <input type="text" class="profile-form-input-custome" placeholder="Pin Code*">
-                            </div>
+                            <h5 class="main-head text-red">Add Address</h5>
 
                             <div class="form-group py-2">
-                                <input type="text" class=" profile-form-input-custome" placeholder="Enter name">
-                            </div>
-
-                            <div class="form-group py-2">
-                                <input type="text" class=" profile-form-input-custome" placeholder="Address*">
-                            </div>
-
-                            <div class="pt-5 pb-3">
-                                <hr class="m-0">
-                            </div>
-
-
-
-                            <div class="form-group py-2">
-                                <input type="text" class=" profile-form-input-custome" placeholder="Landmark">
+                                <input type="text" class=" profile-form-input-custome" placeholder="Street Address*">
                             </div>
 
                             <div class="form-group py-2">
@@ -195,15 +175,11 @@
                             <div class="form-group py-2">
                                 <input type="text" class=" profile-form-input-custome" placeholder="Country">
                             </div>
-
-                            <div class="form-group py-2">
-                                <input type="text" class=" profile-form-input-custome" placeholder="Mobile No*">
-
+                            <div class="form-group  profile-form-group-star-pin-code py-2">
+                                <input type="text" class="profile-form-input-custome" placeholder="Pin Code*">
                             </div>
 
-
-
-                            <div class="row">
+                            {{-- <div class="row">
                                 <div class="col-sm-6 py-2">
 
                                     <label for="" class="profile-f-l-color"> Address Type</label>
@@ -217,7 +193,7 @@
                                         <option value="2" class="profile-f-l-color">Other</option>
                                     </select>
                                 </div>
-                            </div>
+                            </div> --}}
 
 
                             <div class="col-sm-12 pt-4">
