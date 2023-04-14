9d3c9547cbba343b861565f03b2eba6237e1259e
Kishan-Yadav-Acetrot <98552345+Acetrot-Kishan@users.noreply.github.com>
2023-04-14 12:30:13 +0530

    favicon update

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,3 +1,9 @@
+<link href="{{ url('frontend/images/favicon.png') }}" rel="shortcut icon" type="image/x-icon">
+<link href="{{ url('frontend/images/favicon.png') }}" rel="apple-touch-icon" sizes="144x144">
+<link href="{{ url('frontend/images/favicon.png') }}" rel="apple-touch-icon" sizes="114x114">
+<link href="{{ url('frontend/images/favicon.png') }}" rel="apple-touch-icon" sizes="72x72">
+<link href="{{ url('frontend/images/favicon.png') }}" rel="apple-touch-icon">
+
 <!-- include the site stylesheet -->
 <link
     href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic%7cMontserrat:400,700%7cOxygen:400,300,700'
@@ -6,7 +12,7 @@
 <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
 
 <link rel="stylesheet" href="{{ asset('frontend/css/fontawesome.min.css') }}">
-<link rel="shortcut icon" href="{{ asset('frontend/images/favicon.png') }}" type="image/x-icon">
+
 <!-- include the site stylesheet -->
 <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
 <!-- include the site stylesheet -->
