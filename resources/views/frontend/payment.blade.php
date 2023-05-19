a36713d0009c983df3ba9fc3ef39008b4a17f325
Raees-Shaikh-Acetrot <raees.shaikh@acetrot.com>
2023-05-19 16:08:17 +0530

    credit & debit card payment form created updated

--- resources/views/frontend/payment.blade.php
+++ resources/views/frontend/payment.blade.php
@@ -92,9 +92,95 @@
                             </h2>
                             <div id="flush-collapseFour" class="accordion-collapse collapse"
                                 aria-labelledby="flush-headingfour" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">Placeholder content for this accordion, which is intended to
-                                    demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion
-                                    body. Let's imagine this being filled with some actual content.</div>
+                                <div class="accordion-body">
+
+                                    <div class="container">
+                                        <div class="row">
+                                            <!-- You can make it whatever width you want. I'm making it full width
+                                            on <= small devices and 4/12 page width on >= medium devices -->
+                                            <div class="col-xs-12 col">
+
+
+                                                <!-- CREDIT CARD FORM STARTS HERE -->
+                                                <div class="panel panel-default credit-card-box">
+                                                    <div class="panel-heading display-table">
+                                                        <div class="row display-tr">
+                                                            <h3 class="panel-title display-td text-center">Payment Details
+                                                            </h3>
+
+                                                        </div>
+                                                    </div>
+                                                    <div class="panel-body">
+                                                        <form role="form" id="payment-form">
+                                                            <div class="row mt-3">
+                                                                <div class="col-xs-12">
+                                                                    <div class="form-group">
+                                                                        <label for="cardNumber">Card Number</label>
+                                                                        <div class="input-group">
+                                                                            <input type="tel" class="form-control"
+                                                                                name="cardNumber"
+                                                                                placeholder="Valid Card Number"
+                                                                                autocomplete="cc-number" required
+                                                                                autofocus />
+
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row mt-3">
+                                                                <div class="col-xs-7 col-md-7">
+                                                                    <div class="form-group">
+                                                                        <label for="cardExpiry"><span
+                                                                                class="hidden-xs">Expiration</span>Date</label>
+                                                                        <input type="tel" class="form-control"
+                                                                            name="cardExpiry" placeholder="MM / YY"
+                                                                            autocomplete="cc-exp" required />
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-xs-5 col-md-5 pull-right">
+                                                                    <div class="form-group">
+                                                                        <label for="cardCVC">CV CODE</label>
+                                                                        <input type="tel" class="form-control"
+                                                                            name="cardCVC" placeholder="CVC"
+                                                                            autocomplete="cc-csc" required />
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row mt-3">
+                                                                <div class="col-xs-12">
+                                                                    <div class="form-group">
+                                                                        <label for="couponCode"> Coupon Code</label>
+                                                                        <input type="text" class="form-control"
+                                                                            name="couponCode" />
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-xs-12 text-center">
+                                                                    <button
+                                                                        class="btn btn- btn-lg btn-block mx-auto text-center mt-3 pay-now"
+                                                                        type="submit">Pay Now</button>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row" style="display:none;">
+                                                                <div class="col-xs-12">
+                                                                    <p class="payment-errors"></p>
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                                <!-- CREDIT CARD FORM ENDS HERE -->
+
+
+                                            </div>
+
+
+
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
