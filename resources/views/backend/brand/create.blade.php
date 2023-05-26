b8f464e180415142ca9371e42d3b2e721e2a554e
Raees-Shaikh-Acetrot <raees.shaikh@acetrot.com>
2023-05-26 16:25:37 +0530

    brand, create & edit form responsive updated

--- resources/views/backend/brand/create.blade.php
+++ resources/views/backend/brand/create.blade.php
@@ -6,32 +6,32 @@
             <div class="statbox widget box box-shadow my-1">
                 <div class="widget-header">
                     <div class="row justify-content-between align-items-center ">
-                        <div class="col-lg-4 col-md-6 col-sm-6 mt-2 mb-2 ">
+                        <div class="col-xl-4 col-md-6 col-sm-6 mt-2 mb-2 ">
                             <legend class="h4">
                                 Create Brand
                             </legend>
                         </div>
 
-                        <div class="col-lg-4 col-md-6 col-sm-6 mb-2 d-flex justify-content-end align-it mt-2">
+                        <div class="col-xl-4 col-md-6 col-sm-6 mb-2 d-flex justify-content-end align-it mt-2">
                             <nav class="breadcrumb-two" aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="/">Home</a></li>
                                     <li class="breadcrumb-item active" aria-current="page"><a
-                                            href="javascript:void(0);">Brand</a></li>
+                                            href="javascript:void(0);">Create Brand</a></li>
                                 </ol>
                             </nav>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="statbox widget box box-shadow temp-a col-md-6">
+            <div class="statbox widget box box-shadow temp-a col-xl-6">
                 <div class="row m-0">
                     <div class="col-12">
                         <form class="mt-3" method="POST" action="{{ route('backend.brand.store') }}"
                             enctype="multipart/form-data" autocomplete="off">
                             @csrf
                             <div class="form-group mb-4 row">
-                                <div class="col-xl-12 col-lg-4 col-md-6 col-sm-12">
+                                <div class="col-xl-12  col-md-6 col-sm-12">
                                     <label for="formGroupExampleInput" class="">Name</label>
                                     <input type="text" class="form-control" id="formGroupExampleInput"
                                         placeholder="Enter Name" minlength="3" maxlength="40" required name="name" value="{{ old('name') }}">
@@ -39,7 +39,7 @@
                                         <div class="text-danger" role="alert">{{ $errors->first('name') }}</div>
                                     @endif
                                 </div>
-                                <div class="col-xl-12 col-lg-4 col-md-6 col-sm-12 py-2">
+                                <div class="col-xl-12  col-md-6 col-sm-12 py-2">
                                     <label for="formGroupExampleInput" class="">Image</label>
                                     <input type="file" class="form-control" id="formGroupExampleInput"required
                                         name="image">
@@ -47,7 +47,7 @@
                                         <div class="text-danger" role="alert">{{ $errors->first('image') }}</div>
                                     @endif
                                 </div>
-                                <div class="col-xl-12 col-lg-4 col-md-6 col-sm-12 py-2">
+                                <div class="col-xl-12  col-md-6 col-sm-12 py-2">
                                     <label for="degree2">Description</label>
                                     <textarea class="form-control" placeholder="Enter Description" rows="3" name="descriptions" minlength="3"
                                         maxlength="250">{{ old('descriptions') }}</textarea>
