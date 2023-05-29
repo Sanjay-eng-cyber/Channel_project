7aa3117011ebf20ebff55de1b8dc7fe6c2ae22e5
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2023-05-29 11:47:47 +0530

    cart error fixes

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend.layouts.app')
-@section('title', 'Wishlist |')
+@section('title', 'Cart |')
 @section('cdn')
 
     <style>
@@ -34,143 +34,149 @@
         </div>
     </section>
 
-    <section style="padding:40px 0px 65px 0px">
-        <div class="container">
-            <h5 class="h5 main-head text-center text-red my-3">Your Paynent Method</h5>
-            <div class="row d-flex
+    @if ($cartItems)
+        <section style="padding:40px 0px 65px 0px">
+            <div class="container">
+                <h5 class="h5 main-head text-center text-red my-3">Your Paynent Method</h5>
+                <div class="row d-flex
             flex-column-reverse flex-md-row">
-                <div class="col-md-8 col-lg-9 __cart-ui-f">
+                    <div class="col-md-8 col-lg-9 __cart-ui-f">
 
-                    <div class="row d-flex justify-content-center justify-content-lg-end __cart-ui-nav">
-                        <div class="col-sm-12 px-0">
-                            <h5 class="main-head __cart-ui-nav-heading">Deselect all items</h5>
-                            <div
-                                class="d-flex cc-border justify-content-between p-3 justify-content-between __cart-ui-nav-des">
-                                <div class="fw-bold">Product</div>
-                                <ul class="d-flex gap-5 list-unstyled p-0 m-0">
-                                    <li class="fw-bold">Price</li>
-                                    <li class="fw-bold">Quantity</li>
-                                    <li class="fw-bold">Total</li>
-                                </ul>
+                        <div class="row d-flex justify-content-center justify-content-lg-end __cart-ui-nav">
+                            <div class="col-sm-12 px-0">
+                                <h5 class="main-head __cart-ui-nav-heading">Deselect all items</h5>
+                                <div
+                                    class="d-flex cc-border justify-content-between p-3 justify-content-between __cart-ui-nav-des">
+                                    <div class="fw-bold">Product</div>
+                                    <ul class="d-flex gap-5 list-unstyled p-0 m-0">
+                                        <li class="fw-bold">Price</li>
+                                        <li class="fw-bold">Quantity</li>
+                                        <li class="fw-bold">Total</li>
+                                    </ul>
 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    @forelse ($cartItems as $c)
-                        <div class="row cc-border my-3 __cart-ui-card" style="padding:15px">
-                            <div class="__cart-ui-close-btn"><button type="button" class="btn-close"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class=" col-md-4 col-lg-3">
-                                <div class="d-flex align-items-center justify-content-center gap-1 __cart-ui-img-in">
-                                    <div class="form-check">
-                                        <input class="form-check-input ck-wallet-fi" type="checkbox" name="gender"
-                                            id="male" value="male" checked="">
+                        @forelse ($cartItems as $c)
+                            <div class="row cc-border my-3 __cart-ui-card" style="padding:15px">
+                                <div class="__cart-ui-close-btn"><button type="button" class="btn-close"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class=" col-md-4 col-lg-3">
+                                    <div class="d-flex align-items-center justify-content-center gap-1 __cart-ui-img-in">
+                                        <div class="form-check">
+                                            <input class="form-check-input ck-wallet-fi" type="checkbox" name="gender"
+                                                id="male" value="male" checked="">
+                                        </div>
+                                        <img src="https://via.placeholder.com/180x180" class="img-fluid img-border"
+                                            alt="">
                                     </div>
-                                    <img src="https://via.placeholder.com/180x180" class="img-fluid img-border"
-                                        alt="">
+                                </div>
+
+                                <div class="col-md-8 col-lg-4   mt-3 mt-md-0 d-flex align-items-center">
+                                    <div class="">
+                                        <h5 class="main-head">{{ $c->product->name }}</h5>
+                                        <p class="p-0 __cart-ui-pra">
+                                            {{ $c->product->short_descriptions }}
+                                        </p>
+
+                                    </div>
+                                </div>
+
+                                <div
+                                    class="col-md-12 col-lg-5 d-flex justify-content-center align-items-center justify-content-between flex-row flex-lg-column flex-xl-row my-3 my-lg-0">
+                                    <ul class="p-0 m-0">
+                                        <li class="no-bullet "><s class="text-muted">{{ $c->product->mrp }}</s></li>
+                                        <li class="no-bullet  "><strong>{{ $c->product->final_price }}</strong></li>
+                                        @if ($c->product->stock)
+                                            <li class="no-bullet text-green">In Stock</li>
+                                        @endif
+                                    </ul>
+                                    <div class="input-group align-items-center" style="width:124px;">
+                                        <span class="input-group-text">-</span>
+                                        <input type="number" id="qty" class="form-control text-center"
+                                            value="1">
+                                        <span class="input-group-text">+</span>
+                                    </div>
+
+                                    <div class="d-none d-md-inline">
+                                        ₹145.55
+                                    </div>
+
+
                                 </div>
                             </div>
+                        @empty
+                        @endforelse
 
-                            <div class="col-md-8 col-lg-4   mt-3 mt-md-0 d-flex align-items-center">
-                                <div class="">
-                                    <h5 class="main-head">{{ $c->product->name }}</h5>
-                                    <p class="p-0 __cart-ui-pra">
-                                        {{ $c->product->short_descriptions }}
-                                    </p>
-
-                                </div>
-                            </div>
-
-                            <div
-                                class="col-md-12 col-lg-5 d-flex justify-content-center align-items-center justify-content-between flex-row flex-lg-column flex-xl-row my-3 my-lg-0">
-                                <ul class="p-0 m-0">
-                                    <li class="no-bullet "><s class="text-muted">{{ $c->product->mrp }}</s></li>
-                                    <li class="no-bullet  "><strong>{{ $c->product->final_price }}</strong></li>
-                                    @if ($c->product->stock)
-                                        <li class="no-bullet text-green">In Stock</li>
-                                    @endif
+                        <div class="pagination col-lg-12">
+                            <div class="col-md-12 text-center align-self-center">
+                                <ul class="pagination text-center">
+                                    {{ $cartItems->appends(Request::all())->links('pagination::bootstrap-4') }}
                                 </ul>
-                                <div class="input-group align-items-center" style="width:124px;">
-                                    <span class="input-group-text">-</span>
-                                    <input type="number" id="qty" class="form-control text-center" value="1">
-                                    <span class="input-group-text">+</span>
-                                </div>
-
-                                <div class="d-none d-md-inline">
-                                    ₹145.55
-                                </div>
-
-
                             </div>
                         </div>
-                    @empty
-                    @endforelse
-                    <div class="pagination col-lg-12">
-                        <div class="col-md-12 text-center align-self-center">
-                            <ul class="pagination text-center">
-                                {{ $cartItems->appends(Request::all())->links('pagination::bootstrap-4') }}
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-md-4 col-lg-3">
-                    <div class="" style="padding: 50px 15px 15px 15px">
-                        <h5 class="main-head">Order Summary</h5>
-                        <hr style="border-bottom: 2px solid #000000;">
-                        <div class="d-flex justify-content-between my-3">
-                            <strong>Sub Total</strong>
-                            <strong> {{ $subTotal }}</strong>
-                        </div>
-                        <hr>
-
-                        <div class="my-2">
-                            <form action="" method="post">
-                                <label for="coupon-code-input my-3">Coupon</label>
-
-                                <div class="input-group my-2">
-                                    <input type="text" class="form-control" id="coupon-code-input"
-                                        placeholder="Enter coupon code" aria-label="Coupon code"
-                                        aria-describedby="coupon-button">
-                                </div>
-                            </form>
-                        </div>
-
-                        <p class="m-0 p-0 my-2 __cart-ui-pra text-capitalize">coupon code will apply on checkout page</p>
-
-                        <div class="d-flex justify-content-between my-3">
-                            <strong>Total:</strong>
-                            <strong>{{ $subTotal }}</strong>
-                        </div>
-                        <hr>
-                        <p class="m-0 p-0 my-2 __cart-ui-pra text-capitalize">coupon code will apply on checkout page</p>
-
-                        <div>
-                            <a href="http://" class="my-2 text-white">
-                                <button type="button" class="btn btn-outline-pink-hover w-100 p-1 p-xl-2">
-                                    Proceed To Checkout
-                                </button>
-                            </a>
-
-                            <a href="http://" class="my-2 text-white">
-                                <button type="button" class="btn btn-orange-outline-hover w-100 my-2  p-1 p-xl-2">
-                                    Proceed To Checkout
-                                </button>
-                            </a>
-                        </div>
-
 
                     </div>
+                    <div class="col-md-4 col-lg-3">
+                        <div class="" style="padding: 50px 15px 15px 15px">
+                            <h5 class="main-head">Order Summary</h5>
+                            <hr style="border-bottom: 2px solid #000000;">
+                            <div class="d-flex justify-content-between my-3">
+                                <strong>Sub Total</strong>
+                                <strong> {{ $subTotal }}</strong>
+                            </div>
+                            <hr>
 
+                            <div class="my-2">
+                                <form action="" method="post">
+                                    <label for="coupon-code-input my-3">Coupon</label>
+
+                                    <div class="input-group my-2">
+                                        <input type="text" class="form-control" id="coupon-code-input"
+                                            placeholder="Enter coupon code" aria-label="Coupon code"
+                                            aria-describedby="coupon-button">
+                                    </div>
+                                </form>
+                            </div>
+
+                            <p class="m-0 p-0 my-2 __cart-ui-pra text-capitalize">coupon code will apply on checkout page
+                            </p>
+
+                            <div class="d-flex justify-content-between my-3">
+                                <strong>Total:</strong>
+                                <strong>{{ $subTotal }}</strong>
+                            </div>
+                            <hr>
+                            <p class="m-0 p-0 my-2 __cart-ui-pra text-capitalize">coupon code will apply on checkout page
+                            </p>
+
+                            <div>
+                                <a href="http://" class="my-2 text-white">
+                                    <button type="button" class="btn btn-outline-pink-hover w-100 p-1 p-xl-2">
+                                        Proceed To Checkout
+                                    </button>
+                                </a>
+
+                                <a href="http://" class="my-2 text-white">
+                                    <button type="button" class="btn btn-orange-outline-hover w-100 my-2  p-1 p-xl-2">
+                                        Proceed To Checkout
+                                    </button>
+                                </a>
+                            </div>
+
+
+                        </div>
+
+                    </div>
                 </div>
+
+
             </div>
 
-
-        </div>
-
-    </section>
-
-    @include('frontend.not-found')
+        </section>
+    @else
+        @include('frontend.not-found')
+    @endif
 
 @endsection
--- resources/views/frontend/wishlist.blade.php
+++ resources/views/frontend/wishlist.blade.php
@@ -60,10 +60,24 @@
                                 </div>
                                 <div
                                     class="col-sm-12  col-md-12 col-lg-12 col-xl-3 py-3 py-xl-0 py-xxl-0 wishlist-main-in-btn">
-                                    <a href="javascript:void(0)"
-                                        class="add-to-cart-btn btn profile-btn-color add-p-btn btn-pink add-to-cart"
-                                        data-p-id="{{ $w->product->id }}">Add To
-                                        Cart</a>
+                                    @if ($w->product->isInCart())
+                                        <a href="javascript:void(0)"
+                                            class="add-p-btn add-to-cart add-to-cart-btn btn btn-outline-pink btn-pink profile-btn-color"
+                                            data-p-id="{{ $w->product->id }}"><svg class="svg-inline--fa fa-check"
+                                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
+                                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                                                data-fa-i2svg="">
+                                                <path fill="currentColor"
+                                                    d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z">
+                                                </path>
+                                            </svg>Added</a>
+                                    @else
+                                        <a href="javascript:void(0)"
+                                            class="add-to-cart-btn btn profile-btn-color add-p-btn btn-pink add-to-cart"
+                                            data-p-id="{{ $w->product->id }}">Add To
+                                            Cart</a>
+                                    @endif
+
                                     <a href="" style="font-size:12px" class="py-1 text-red mt-md-1 mt-2"
                                         data-bs-toggle="modal" data-bs-target="#wishlist-popup">
                                         Remove item from Wishlist
