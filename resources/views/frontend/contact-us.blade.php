8fae2f6ef8b0331356bbb9d4739855b9e65ed614
Raees-Shaikh-Acetrot <raees.shaikh@acetrot.com>
2023-05-30 13:14:22 +0530

    contact form created & updated

--- resources/views/frontend/contact-us.blade.php
+++ resources/views/frontend/contact-us.blade.php
@@ -18,9 +18,9 @@
                     <div class="row main-group-card pt-5 mx-md-4 mb-md-4 contact-img">
                         <h3 class="text-center text-white">Contact Information</h3>
                         <p class="text-white text-center">Get In Touch</p>
-                        <div class="col-lg-6 px-5 py-5">
+                        <div class="col-lg-6 px-md-5 py-5">
 
-                            <ul class="list-unstyled ">
+                            <ul class="list-unstyled mt-sm-4 mt-3">
 
                                 <li class="gap-2 d-flex align-items-center mt-sm-4 mt-3">
                                     <i class="fas fa-phone  text-white"></i>
@@ -38,24 +38,39 @@
 
 
                             </ul>
-                            <ul class="list-unstyled d-flex gap-2 p-0">
+                            <ul class="list-unstyled d-flex gap-2 p-0 mt-sm-4 mt-3">
                                 <li>
                                     <i class="fas fa-map-marker-alt text-white"></i>
                                 </li>
                                 <li class="text-white">
                                     Shop No. 5 & 6, Sunview Apartment, Tilak Nagar, Chembur (West), Mumbai - 400089.
                                 </li>
+
+
+                            </ul>
+                            <ul class="list-unstyled d-flex gap-2 p-0 mt-sm-4 mt-3">
+                                <li class="gap-2 d-flex align-items-center footer-links-hover mt-sm-0 mt-3">
+                                    <i class="fas fa-clock  text-white"></i>
+                                    <span class="text-white">Mon-Sat | 10:00am to 6.00pm</span>
+                                </li>
                             </ul>
                         </div>
-                        <div class="col-lg-5 py-5">
-                            <div class="card py-5">
-                                <i class="fa-regular fa-clock  text-red d-block  f-s-50"></i>
-                                <p class="text-red text-center mt-3 m-0 p-0 mb-2">Timings</p>
-                                <hr class="text-red w-50 mx-auto py-0 my-0">
-                                <p class="text-center text-red py-0 my-0 mt-2">Mon-Sat</p>
-                                <p class="text-center text-red py-0 my-0">
-                                    10:00am to 6.00pm</p>
+                        <div class="col-lg-5 py-5 ">
+                            <div class="card-bg card py-5">
+                                <div class="px-3 ">
+                                    <div class="form-group">
+                                      <label for="exampleFormControlInput1">Email </label>
+                                      <input type="email" class="form-control mt-2" id="exampleFormControlInput1" placeholder="name@example.com">
+                                    </div>
 
+
+                                    <div class="form-group mt-3">
+                                      <label for="exampleFormControlTextarea1">Message</label>
+                                      <textarea class="form-control mt-2" id="exampleFormControlTextarea1" rows="3" placeholder="Write Your Message"></textarea>
+                                    </div>
+
+                                </div>
+                                <button class="btn btn-block btn-parrot mt-3 mx-3">Submit</button>
                             </div>
                         </div>
 
