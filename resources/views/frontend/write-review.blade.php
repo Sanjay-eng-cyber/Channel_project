c961bbf4ee587bce1753af40c5424b6f07b0edbb
Raees-Shaikh-Acetrot <raees.shaikh@acetrot.com>
2023-05-31 18:24:53 +0530

    write-review updated

--- resources/views/frontend/write-review.blade.php
+++ resources/views/frontend/write-review.blade.php
@@ -9,8 +9,8 @@
         <div class="container">
             <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#" class="bread-crum">Profile</a></li>
-                    <li class="breadcrumb-item"><a href="#" class="bread-crum">My Order</a></li>
+                    <li class="breadcrumb-item"><a href="#" class="bread-crum breadcrumb-hover">Profile</a></li>
+                    <li class="breadcrumb-item"><a href="#" class="bread-crum breadcrumb-hover" >My Order</a></li>
                     <li class="breadcrumb-item bread-crum" aria-current="page">Write A Review</li>
                 </ol>
             </nav>
@@ -32,8 +32,8 @@
                         </div>
 
                         <div>
-                            <div class="write-review-fw main-head pt-4 pb-2">Essence Long Lasting Eye Pencil</div>
-                            <div>essence Long Lasting Eye Pencil is a creamy and pigmented eye pencil that brightens and
+                            <div class="write-review-fw main-head pt-4 pb-2 text-capitalize">Essence Long Lasting Eye Pencil</div>
+                            <div class="text-capitalize">essence Long Lasting Eye Pencil is a creamy and pigmented eye pencil that brightens and
                                 accentuates your eye more....</div>
                         </div>
 
@@ -41,7 +41,7 @@
                         <div>
                             <ul class="d-flex flex-column flex-md-row justify-content-between p-0 gap-2 gap-md-0">
                                 <li class="write-review-fw ">Delivered <br /> 19 March 2023</li>
-                                <li class="text-blue write-review-fw">Return order</li>
+                                <li class="text-blue write-review-fw">Return Order</li>
                                 <li class="write-review-fw no-bullet">From ₹145.55</li>
                             </ul>
                         </div>
@@ -81,14 +81,14 @@
 
                         <div class="review-guid-line my-2">
                             <div class="review-guid-line-heading">Review Guidelines</div>
-                            <p>Once written and submitted, reviews are the sole property of channel. and channel reserves
+                            <p class="text-capitalize">Once written and submitted, reviews are the sole property of channel. and channel reserves
                                 the right to refuse
                                 and remove any review/rating.
                                 channel will not be held responsible or accept any liability of reviews posted for any
                                 product/brand.
                                 The opinions expressed in reviews are those of channel users and not of channel.</p>
                             <div class="review-guid-line-heading">Disclaimer</div>
-                            <p>Once written and submitted, reviews are the sole property of channel. and channel reserves
+                            <p class="text-capitalize">Once written and submitted, reviews are the sole property of channel. and channel reserves
                                 the right to refuse
                                 and remove any review/rating.
                                 channel will not be held responsible or accept any liability of reviews posted for any
@@ -121,14 +121,14 @@
             <div class="col-lg-12 col-xl-6 or-secondpage-scard">
                 <div class="p-4 or-secondpage-scard">
 
-                    <h5 class="main-head py-3 or-secondpage-scard-fhead">Recommended based on your purchase</h5>
+                    <h5 class="main-head py-3 or-secondpage-scard-fhead text-capitalize">Recommended based on your purchase</h5>
                     <div class="row pt-3 pb-3 or-secondpage-scard-card">
                         <div class="col-sm-4 or-secondpage-scard-card-img" style="">
                             <img src="frontend/images/products/skin/sk1.png" alt="" class="img-fluid">
                         </div>
                         <div class="col-sm-8 or-secondpage-scard-card-des pt-4 pt-sm-0 pt-md-0">
                             <h4 class="main-head">Essence Long Lasting Eye Pencil</h4>
-                            <p>
+                            <p class="text-capitalize">
                                 essence Long Lasting Eye Pencil is a creamy and pigmented eye pencil that brightens
                                 and
                                 accentuates your eye more....
