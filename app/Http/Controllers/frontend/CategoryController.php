dd26bca3213bf7b4e474b5a6eab8d7ad72d3af15
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2023-06-23 11:45:05 +0530

    price filter done in category frontend

--- app/Http/Controllers/frontend/CategoryController.php
+++ app/Http/Controllers/frontend/CategoryController.php
@@ -22,13 +22,12 @@ class CategoryController extends Controller
         $products = $products->latest();
         $products = $this->filterResults($request, $products);
         $products = $products->paginate(10);
-       // dd($products);
+        //dd($products);
         return view('frontend.product.index', compact('products', 'category'));
     }
 
     protected function filterResults($request, $products)
     {
-       //dd($request);
         if ($request->q !== '' && !is_null($request->q)) {
             if (is_numeric($request->q)) {
                 $request->validate(['q' => 'digits_between:1,40'], ['q.*' => 'Please enter proper Number']);
@@ -36,7 +35,13 @@ class CategoryController extends Controller
                 $request->validate(['q' => 'min:3']);
             }
         }
-        if ($request !== null && $request->has('q') && $request->q = 1) {
+        if ($request !== null && $request->has('q') && $request['q'] == 1) {
+            // dd('kddfk');
+            $products = $products->orderBy('mrp', 'asc');
+            //dd($products);
+        }
+        //dd($request->q);
+        if ($request !== null && $request->has('q') && $request['q'] == 2) {
             $products = $products->orderBy('mrp', 'desc');
             //dd($products);
         }
