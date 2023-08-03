4e3d7096ff779c55f0edc217c6c83d3994c80c95
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-08-03 19:19:19 +0530

    contact page updated

--- resources/views/frontend/contact-us.blade.php
+++ resources/views/frontend/contact-us.blade.php
@@ -1,32 +1,24 @@
 @extends('frontend.layouts.app')
 @section('title', 'Contact |')
-<style>
-    .contact-border .contact-img {
-        background: url('frontend/images/banner/contact-bg.png') !important;
-        background-repeat: no-repeat !important;
-        background-size: cover !important;
-        border-radius: 10px;
-
-    }
-</style>
 @section('content')
     {{-- first slider slider --}}
     <section class="pb-3">
         <div class="container">
             <div class="row justify-content-center my-5 px-sm-0 px-2">
 
-                <div class="col-md-12 p-md-5 p-4 contact-border ">
+                <div class="col-xl-12 col-xxl-11 p-3 p-sm-4 p-md-5 contact-border ">
                     <h2 class="text-red main-head text-center mb-3">Contact US</h2>
-                    <div class="card pt-md-4">
-                        <div class="row main-group-card pt-5 mx-md-4 mb-md-4 contact-img ">
-                            <h3 class="text-center text-white">Contact Information</h3>
+                        <div class="row main-group-card p-2 p-sm-4 contact-img ">
+                            <h3 class="text-center text-white font-body">Contact Information</h3>
                             <p class="text-white text-center">Get In Touch</p>
-                            <div class="col-lg-6 px-md-5 py-md-5">
+
+                            <div class="col-lg-6  pt-md-5">
 
                                 <ul class="list-unstyled mt-sm-4 mt-3">
 
                                     <li class="gap-2 d-flex align-items-center mt-sm-4 mt-3">
                                         <i class="fas fa-phone  text-white"></i>
+
                                         <a href="tel:+917710062724"
                                             class="contact-us-link footer-links-hover text-white">+91-7710062724</a>
                                     </li>
@@ -47,7 +39,7 @@
                                         <i class="fas fa-map-marker-alt text-white"></i>
                                     </li>
                                     <li class="text-white">
-                                        Shop No. 5 & 6, Sunview Apartment, Tilak Nagar, Chembur (West), Mumbai - 400089.
+                                        Shop No. 5 & 6, Sunview Apartment,<br/> Tilak Nagar, Chembur (West), Mumbai - 400089.
                                     </li>
 
 
@@ -59,63 +51,80 @@
                                     </li>
                                 </ul>
                             </div>
-                            <div class="col-lg-5 py-5 ">
-                                <div class="card-bg card py-5">
+
+                            <div class="col-lg-5 py-2 isolation-contact position-relative">
+
+                                <img src="{{ asset('frontend/images/svg/first-circle-contact.svg') }}" alt="img" class="img-fluid circle-first-contact">
+
+                                <img src="{{ asset('frontend/images/svg/second-circle-contact.svg') }}" alt="img" class="img-fluid circle-second-contact">
+                                <div class="card-bg card p-3">
                                     <form action="{{ route('frontend.contact.submit') }}" method="POST">
                                         @csrf
-                                        <div class="px-3 ">
-                                            <div class="form-group">
-                                                <label for="exampleFormControlInput1">Name </label>
-                                                <input type="text" class="form-control mt-2"
-                                                    id="exampleFormControlInput1" placeholder="Enter Your Name"
-                                                    name="name" minlength="3" maxlength="30" value="{{old('name')}}" required>
-                                                @if ($errors->has('name'))
-                                                    <div class="text-danger" role="alert">
-                                                        {{ $errors->first('name') }}
-                                                    </div>
-                                                @endif
+
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlInput1">Name </label>
+                                                    <input type="text" class="form-control mt-2"
+                                                        id="exampleFormControlInput1" placeholder="Name"
+                                                        name="name" minlength="3" maxlength="30" value="{{old('name')}}" required>
+                                                    @if ($errors->has('name'))
+                                                        <div class="text-danger" role="alert">
+                                                            {{ $errors->first('name') }}
+                                                        </div>
+                                                    @endif
+                                                </div>
                                             </div>
-                                            <div class="form-group mt-3">
-                                                <label for="exampleFormControlInput1">Email </label>
-                                                <input type="email" class="form-control mt-2"
-                                                    id="exampleFormControlInput1" placeholder="Enter Your Email"
-                                                    name="email" minlength="5" maxlength="40" value="{{old('email')}}" required>
-                                                @if ($errors->has('email'))
-                                                    <div class="text-danger" role="alert">
-                                                        {{ $errors->first('email') }}
-                                                    </div>
-                                                @endif
+
+                                            <div class="col-12 col-md-6">
+                                                <div class="form-group mt-3 mt-md-0">
+                                                    <label for="exampleFormControlInput1">Phone No. </label>
+                                                    <input type="text" class="form-control mt-2"
+                                                        id="exampleFormControlInput1" placeholder="Phone Number"
+                                                        name="phone" minlength="10" maxlength="10" value="{{old('phone')}}" required>
+                                                    @if ($errors->has('phone'))
+                                                        <div class="text-danger" role="alert">
+                                                            {{ $errors->first('phone') }}
+                                                        </div>
+                                                    @endif
+                                                </div>
                                             </div>
-                                            <div class="form-group mt-3">
-                                                <label for="exampleFormControlInput1">Phone No. </label>
-                                                <input type="text" class="form-control mt-2"
-                                                    id="exampleFormControlInput1" placeholder="Enter Your Phone No."
-                                                    name="phone" minlength="10" maxlength="10" value="{{old('phone')}}" required>
-                                                @if ($errors->has('phone'))
-                                                    <div class="text-danger" role="alert">
-                                                        {{ $errors->first('phone') }}
-                                                    </div>
-                                                @endif
+
+                                            <div class="col-12">
+                                                <div class="form-group mt-3">
+                                                    <label for="exampleFormControlInput1">Email </label>
+                                                    <input type="email" class="form-control mt-2"
+                                                        id="exampleFormControlInput1" placeholder="Enter Your Email"
+                                                        name="email" minlength="5" maxlength="40" value="{{old('email')}}" required>
+                                                    @if ($errors->has('email'))
+                                                        <div class="text-danger" role="alert">
+                                                            {{ $errors->first('email') }}
+                                                        </div>
+                                                    @endif
+                                                </div>
                                             </div>
-                                            <div class="form-group mt-3">
-                                                <label for="exampleFormControlTextarea1">Message</label>
-                                                <textarea class="form-control mt-2" id="exampleFormControlTextarea1" rows="3" placeholder="Write Your Message"
-                                                    name="message" minlength="5" maxlength="250" required>{{old('message')}}</textarea>
-                                                @if ($errors->has('message'))
-                                                    <div class="text-danger" role="alert">
-                                                        {{ $errors->first('message') }}
-                                                    </div>
-                                                @endif
+
+                                            <div class="col-12">
+                                                <div class="form-group mt-3">
+                                                    <label for="exampleFormControlTextarea1">Message</label>
+                                                    <textarea class="form-control mt-2" id="exampleFormControlTextarea1" rows="3" placeholder="Write Your Message"
+                                                        name="message" minlength="5" maxlength="250" required>{{old('message')}}</textarea>
+                                                    @if ($errors->has('message'))
+                                                        <div class="text-danger" role="alert">
+                                                            {{ $errors->first('message') }}
+                                                        </div>
+                                                    @endif
+                                                </div>
                                             </div>
 
                                         </div>
-                                        <button class="btn btn-block btn-parrot mt-3 mx-3" type="submit">Submit</button>
+
+                                        <button class="btn btn-block btn-parrot  w-100 mt-3" type="submit">Submit</button>
                                     </form>
                                 </div>
                             </div>
 
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
