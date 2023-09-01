f7fe6982f8ff1d704c83ded79efb15c61fe66f9f
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-09-01 13:38:38 +0530

    asterisk in input field fix in profile page

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -6,6 +6,12 @@
 @endsection
 @section('content')
 
+<style>
+    .new-asterisk{
+        color:red;
+    }
+</style>
+
     <x-frontend.profile-nav image="https://via.placeholder.com/300" name="{{ $user->first_name }}" />
 
     <section class="my-1">
@@ -184,17 +190,20 @@
                                 @csrf
                                 <h5 class="main-head text-red">Add Address</h5>
 
-                                <div class="form-group py-2 req-input">
-                                    <input type="text" name="name" class=" profile-form-input-custome "
+                                <div class="form-group py-2 req-input position-relative isolation">
+                                    <input type="text" name="name" class=" profile-form-input-custome profile-asterisk"
                                         placeholder="Name" required minlength="3" maxlength="20" required>
+                                    <span class="new-asterisk position-absolute" style="top:10px;left:50px">*</span>
                                     @if ($errors->has('name'))
                                         <div id="name-error" class="text-primary">
                                             {{ $errors->first('name') }}</div>
                                     @endif
                                 </div>
-                                <div class="form-group py-2 req-input-2">
-                                    <input type="text" name="street_address" class=" profile-form-input-custome"
+                                <div class="form-group py-2 req-input-2 position-relative isolation">
+                                    <input type="text" name="street_address" class=" profile-form-input-custome profile-asterisk"
                                         placeholder="Street Address" required minlength="5" maxlength="80" required>
+                                        <span class="new-asterisk  position-absolute" style="top:10px;left:120px">*</span>
+
                                     @if ($errors->has('street_address'))
                                         <div id="street_address-error" class="text-primary">
                                             {{ $errors->first('street_address') }}</div>
@@ -202,7 +211,7 @@
                                 </div>
 
                                 <div class="form-group py-2">
-                                    <input type="text" name="city" class=" profile-form-input-custome"
+                                    <input type="text" name="city" class=" profile-form-input-custome profile-asterisk"
                                         placeholder="City" minlength="3" maxlength="20" required>
                                     @if ($errors->has('city'))
                                         <div id="city-error" class="text-primary">{{ $errors->first('city') }}</div>
@@ -218,16 +227,17 @@
                                 </div>
 
                                 <div class="form-group py-2">
-                                    <input type="text" name="country" class=" profile-form-input-custome"
+                                    <input type="text" name="country" class=" profile-form-input-custome profile-asterisk"
                                         placeholder="Country" minlength="3" maxlength="50" required>
                                     @if ($errors->has('country'))
                                         <div id="country-error" class="text-primary">{{ $errors->first('country') }}
                                         </div>
                                     @endif
                                 </div>
-                                <div class="form-group  profile-form-group-star-pin-code py-2">
-                                    <input type="text" name="postal_code" class="profile-form-input-custome"
+                                <div class="form-group  profile-form-group-star-pin-code py-2 position-relative isolation">
+                                    <input type="text" name="postal_code" class="profile-form-input-custome profile-asterisk"
                                         placeholder="Pin Code" minlength="3" maxlength="20" required>
+                                        <span class="new-asterisk  position-absolute" style="top:10px;left:73px">*</span>
                                     @if ($errors->has('postal_code'))
                                         <div id="postal_code-error" class="text-primary">
                                             {{ $errors->first('postal_code') }}
@@ -381,6 +391,26 @@
     {{-- edit address modal end --}}
 @endsection
 @section('js')
+
+    <script>
+        // Select all input fields with the "required" class
+const requiredInputs = document.querySelectorAll('.profile-asterisk');
+// console.log(requiredInputs);
+
+
+requiredInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+        const asterisk = input.nextElementSibling;
+
+        if (input.value.trim() !== "") {
+            asterisk.style.display = "none";
+        } else {
+            asterisk.style.display = "inline";
+        }
+    });
+});
+
+    </script>
     <script>
         $(document).ready(function() {
             $(document).on('click', '.edit_modal', function() {
