910d1194d35ad56cb524e4e786f5a42e89ddb78e
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2023-09-02 11:37:43 +0530

    slider index issue fixes

--- resources/views/backend/slider/index.blade.php
+++ resources/views/backend/slider/index.blade.php
@@ -108,7 +108,7 @@
                                         </tr>
                                     @empty
                                         <tr>
-                                            <td colspan="4">No Records Found</td>
+                                            <td colspan="5">No Records Found</td>
                                         </tr>
                                     @endforelse
                                 </tbody>
