0d9f7144de9ea7f17c5a72b4936c93125c712350
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2023-09-07 14:23:51 +0530

    logging the webhook response for testing

--- app/Http/Controllers/cms/DeliveryController.php
+++ app/Http/Controllers/cms/DeliveryController.php
@@ -154,9 +154,15 @@ class DeliveryController extends Controller
         $delivery = Delivery::where('status', '!=', 'Delivered')->findOrFail($id);
         // dd($delivery);
         $token = getShiprocketToken();
+        $time = now()->format('d-m-y h:is');
         $response =  Shiprocket::track($token)->throwShipmentId($delivery->shipment_id);
+        Log::info('ShipRocket FetchDelivery ThrowShipmentId Response @ ' . $time);
+        Log::info($response);
         // dd($response);
+        $time = now()->format('d-m-y h:is');
         $shipment = Shiprocket::shipment($token)->getSpecific($delivery->shipment_id);
+        Log::info('ShipRocket FetchDelivery getSpecific Response @ ' . $time);
+        Log::info($shipment);
         // dd($shipment['data']);
         if ($shipment && isset($shipment['data'])) {
             $delivery->update([
