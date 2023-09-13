dfa93bda35a1217b18f46066a3472876f6902ef6
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2023-09-13 12:42:00 +0530

    debug

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::domain(config('app.web_domain'))->group(function () {
 
     Route::get('/test', function () {
+
+        print_r(config('shiprocket.credentials'));
         return getShiprocketToken();
 
         // dd(getShiprocketToken());
