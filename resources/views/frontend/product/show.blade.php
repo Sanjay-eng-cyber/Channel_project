23c1852c7fc4d39bafee03a95e13f2e1640a2905
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-09-14 18:39:54 +0530

    write a review reverse

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -255,10 +255,74 @@
             <div class="container">
 
                 <div class="row">
-                    <div class="my-0 my-lg-2 col-lg-6 col-md-12 product-showpagepills pills-divider-border divider-border-left">
 
-                        <div class="pills-divider d-flex justify-content-center align-items-center ">
-                            <h5 class="main-head tab-fs text-center pills-divider-h5 p-2">Customer Reviews</h5>
+
+                    <div class="my-0 py-3 py-lg-0 my-lg-2 col-lg-6 col-md-12 product-showpagepills pills-divider-border">
+                        <div class=" d-flex justify-content-start ">
+                            <h5 class="main-head tab-fs py-2 pills-divider-h5">Write a Reviews</h5>
+                        </div>
+
+                        <div class="tab-content my-4" >
+
+                            <div class="">
+                                <form action="{{ route('frontend.review.store', $product->slug) }}" method="POST">
+                                    @csrf
+                                    <div class="d-flex justify-content-between flex-column flex-sm-row gap-1">
+                                        <div>Give Your Rating</div>
+
+                                        <div class="rating-input">
+                                            <input type="radio" id="star5" name="rating" value="5" />
+                                            <label class="star" for="star5" title="Awesome"
+                                                aria-hidden="true"></label>
+                                            <input type="radio" id="star4" name="rating" value="4" />
+                                            <label class="star" for="star4" title="Great"
+                                                aria-hidden="true"></label>
+                                            <input type="radio" id="star3" name="rating" value="3" />
+                                            <label class="star" for="star3" title="Very good"
+                                                aria-hidden="true"></label>
+                                            <input type="radio" id="star2" name="rating" value="2" />
+                                            <label class="star" for="star2" title="Good"
+                                                aria-hidden="true"></label>
+                                            <input type="radio" id="star1" name="rating" value="1" />
+                                            <label class="star" for="star1" title="Bad"
+                                                aria-hidden="true"></label>
+                                        </div>
+                                    </div>
+                                    @if ($errors->has('rating'))
+                                        <div class="text-danger text-end" role="alert">{{ $errors->first('rating') }}
+                                        </div>
+                                    @endif
+
+                                    <div class="py-4">
+                                        <input type="text"
+                                            class="form-control my-2 review-sub-headline review-input-bg"
+                                            placeholder="Enter Title" name="title" required
+                                            value="{{ old('title') }}">
+                                        @if ($errors->has('title'))
+                                            <div class="text-danger" role="alert">{{ $errors->first('title') }}
+                                            </div>
+                                        @endif
+                                        <textarea name="body" id="body" cols="10" rows="3"
+                                            class="mt-3 form-control w-100  review-sub-textarea review-input-bg" placeholder="Enter Your Review"
+                                            minlength="3" maxlength="2000" required>{{ old('body') }}</textarea>
+                                        @if ($errors->has('body'))
+                                            <div class="text-danger" role="alert">{{ $errors->first('body') }}
+                                            </div>
+                                        @endif
+                                    </div>
+                                    <div class="text-end">
+                                        <button type="submit" class="btn btn-pink">Post Your Review</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="my-0 my-lg-2 col-lg-6 col-md-12 product-showpagepills pills-divider-border ">
+
+                        <div class="pills-divider d-flex justify-content-start">
+                            <h5 class="main-head tab-fs pills-divider-h5 py-2">Customer Reviews</h5>
                         </div>
 
 
@@ -321,123 +385,11 @@
                                 </div>
                             </div>
 
-                            {{-- <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                <form action="{{ route('frontend.review.store', $product->slug) }}" method="POST">
-                                    @csrf
-                                    <div class="d-flex justify-content-between flex-column flex-sm-row gap-1">
-                                        <div>Give Your Rating</div>
 
-                                        <div class="rating-input">
-                                            <input type="radio" id="star5" name="rating" value="5" />
-                                            <label class="star" for="star5" title="Awesome"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star4" name="rating" value="4" />
-                                            <label class="star" for="star4" title="Great"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star3" name="rating" value="3" />
-                                            <label class="star" for="star3" title="Very good"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star2" name="rating" value="2" />
-                                            <label class="star" for="star2" title="Good"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star1" name="rating" value="1" />
-                                            <label class="star" for="star1" title="Bad"
-                                                aria-hidden="true"></label>
-                                        </div>
-                                    </div>
-                                    @if ($errors->has('rating'))
-                                        <div class="text-danger text-end" role="alert">{{ $errors->first('rating') }}
-                                        </div>
-                                    @endif
-
-                                    <div class="py-4">
-                                        <input type="text"
-                                            class="form-control my-2 review-sub-headline review-input-bg"
-                                            placeholder="Enter Title" name="title" required
-                                            value="{{ old('title') }}">
-                                        @if ($errors->has('title'))
-                                            <div class="text-danger" role="alert">{{ $errors->first('title') }}
-                                            </div>
-                                        @endif
-                                        <textarea name="body" id="body" cols="10" rows="3"
-                                            class="mt-3 form-control w-100  review-sub-textarea review-input-bg" placeholder="Enter Your Review"
-                                            minlength="3" maxlength="2000" required>{{ old('body') }}</textarea>
-                                        @if ($errors->has('body'))
-                                            <div class="text-danger" role="alert">{{ $errors->first('body') }}
-                                            </div>
-                                        @endif
-                                    </div>
-                                    <div class="text-end">
-                                        <button type="submit" class="btn btn-pink">Post Your Review</button>
-                                    </div>
-                                </form>
-                            </div> --}}
 
                         </div>
                     </div>
 
-
-                    <div class="my-0 py-3 py-lg-0 my-lg-2 col-lg-6 col-md-12 product-showpagepills pills-divider-border">
-                        <div class=" d-flex justify-content-center align-items-center ">
-                            <h5 class="main-head tab-fs text-center p-2 pills-divider-h5">Write a Reviews</h5>
-                        </div>
-
-                        <div class="tab-content my-4" >
-
-                            <div class="">
-                                <form action="{{ route('frontend.review.store', $product->slug) }}" method="POST">
-                                    @csrf
-                                    <div class="d-flex justify-content-between flex-column flex-sm-row gap-1">
-                                        <div>Give Your Rating</div>
-
-                                        <div class="rating-input">
-                                            <input type="radio" id="star5" name="rating" value="5" />
-                                            <label class="star" for="star5" title="Awesome"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star4" name="rating" value="4" />
-                                            <label class="star" for="star4" title="Great"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star3" name="rating" value="3" />
-                                            <label class="star" for="star3" title="Very good"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star2" name="rating" value="2" />
-                                            <label class="star" for="star2" title="Good"
-                                                aria-hidden="true"></label>
-                                            <input type="radio" id="star1" name="rating" value="1" />
-                                            <label class="star" for="star1" title="Bad"
-                                                aria-hidden="true"></label>
-                                        </div>
-                                    </div>
-                                    @if ($errors->has('rating'))
-                                        <div class="text-danger text-end" role="alert">{{ $errors->first('rating') }}
-                                        </div>
-                                    @endif
-
-                                    <div class="py-4">
-                                        <input type="text"
-                                            class="form-control my-2 review-sub-headline review-input-bg"
-                                            placeholder="Enter Title" name="title" required
-                                            value="{{ old('title') }}">
-                                        @if ($errors->has('title'))
-                                            <div class="text-danger" role="alert">{{ $errors->first('title') }}
-                                            </div>
-                                        @endif
-                                        <textarea name="body" id="body" cols="10" rows="3"
-                                            class="mt-3 form-control w-100  review-sub-textarea review-input-bg" placeholder="Enter Your Review"
-                                            minlength="3" maxlength="2000" required>{{ old('body') }}</textarea>
-                                        @if ($errors->has('body'))
-                                            <div class="text-danger" role="alert">{{ $errors->first('body') }}
-                                            </div>
-                                        @endif
-                                    </div>
-                                    <div class="text-end">
-                                        <button type="submit" class="btn btn-pink">Post Your Review</button>
-                                    </div>
-                                </form>
-                            </div>
-
-                        </div>
-                    </div>
 
 
                 </div>
