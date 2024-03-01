7e27965f43b2a33a78337b928b66ef7c6cc2b337
Raeesh-Rayeen-Acetrot <raeesh.rayeen@acetrot.com>
2024-03-01 14:50:25 +0530

    tittle fix mail template

--- resources/views/mail/order-placed-mail.blade.php
+++ resources/views/mail/order-placed-mail.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Order Placed</title>
+    <title>Order|Placed</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
